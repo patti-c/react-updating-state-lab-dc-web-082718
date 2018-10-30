bda5ac6981f22660d9f5a52da6148b3e57e7c73d
patti-c <patricia.carter@flatironschool.com>
2018-10-30 10:12:41 -0400

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,47 @@
-// Code DigitalClicker Component Here
+import React, { Component } from 'react';
+
+const colorsArray = ['red', 'orange', 'yellow', 'green', 'blue', 'violet']
+
+export default class DigitalClicker extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      timesClicked: 0,
+      style: {
+        color: colorsArray[0]
+      },
+      colorNumber: 0
+    }
+  }
+
+  handleClick = (e) => {
+    const augment = ++this.state.timesClicked;
+    let newColorNumber = ++this.state.colorNumber;
+    if(newColorNumber > 5) {
+      newColorNumber = 0;
+      this.state.colorNumber = 0;
+    }
+
+
+    this.setState({
+      timesClicked: augment,
+      style: {
+        color: colorsArray[newColorNumber]
+      }
+    })
+  }
+
+  render() {
+    return(
+      <div>
+        DigitalClicker
+        <br></br>
+        <button style={this.state.style} onClick={this.handleClick}>
+          {this.state.timesClicked}
+        </button>
+
+      </div>
+    )
+  }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,64 @@
-// Code YouTubeDebugger Component Here
+import React, { Component } from 'react';
+
+export default class YouTubeDebugger extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      errors: [],
+      user: null,
+      settings: {
+        bitrate: 8,
+        video: {
+          resolution: '1080p'
+        }
+      }
+    }
+  }
+
+  changeBitrate = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        bitrate: 12
+      }
+    })
+  }
+
+  changeResolution = () => {
+    this.setState({
+      settings: {
+        ...this.state.settings,
+        video: {
+          resolution: '720p'
+        }
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>YoutubeDebugger
+        <br></br>
+        <button
+          className="bitrate"
+          onClick={this.changeBitrate}
+        >
+          Change Bitrate
+        </button>
+        <button
+          className="resolution"
+          onClick={this.changeResolution}
+        >
+          Change Resolution
+        </button>
+        <div>
+          Bitrate: {this.state.settings.bitrate}
+          <br></br>
+          Resolution: {this.state.settings.video.resolution}
+        </div>
+      </div>
+
+    )
+  }
+}
